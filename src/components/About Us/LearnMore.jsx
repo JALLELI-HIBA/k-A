23f04d89818d8a4e23e47443fd45dc71cbc12416
amenybettaher <bettaherameni1919@gmail.com>
2025-02-12 21:51:26 +0100

    add some changes

--- src/components/About Us/LearnMore.jsx
+++ src/components/About Us/LearnMore.jsx
@@ -26,26 +26,33 @@ const LearnMorePage = () => {
           <p>
             At K&A, we are committed to realizing a vision of unparalleled luxury by facilitating the import and export of exclusive products that epitomize the artistry, natural beauty, and rich heritage of both Saudi Arabia and France.
           </p>
-          <h3>Mission Statement</h3>
-          <p>
-            Our mission is to connect artisans from Saudi Arabia and France, offering a curated selection of goods that showcase their artistry and authenticity.
-          </p>
-          <h3>Vision Statement</h3>
-          <p>
-            We aspire to be the leading platform for cultural exchange through artisanal goods, creating lasting connections and fostering appreciation for the artistry of both cultures.
-          </p>
+          
+          {/* Mission Statement Card */}
+          <div className="card mission-card">
+            <h3>Mission Statement</h3>
+            <p>
+              Our mission is to connect artisans from Saudi Arabia and France, offering a curated selection of goods that showcase their artistry and authenticity.
+            </p>
+          </div>
+          
+          {/* Vision Statement Card */}
+          <div className="card vision-card">
+            <h3>Vision Statement</h3>
+            <p>
+              We aspire to be the leading platform for cultural exchange through artisanal goods, creating lasting connections and fostering appreciation for the artistry of both cultures.
+            </p>
+          </div>
         </div>
-        
+
         {/* Images side by side */}
         <div className="learn-more-images">
-  <div className="learn-more-image">
-    <img src={pic1} alt="Pic 1" className="right-image" />
-  </div>
-  <div className="learn-more-image">
-    <img src={pic2} alt="Pic 2" className="right-image" />
-  </div>
-</div>
-
+          <div className="learn-more-image">
+            <img src={pic1} alt="Pic 1" className="right-image" />
+          </div>
+          <div className="learn-more-image">
+            <img src={pic2} alt="Pic 2" className="right-image" />
+          </div>
+        </div>
       </div>
     </div>
   );
